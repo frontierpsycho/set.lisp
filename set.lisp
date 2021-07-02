710fcb8ce4e7de9f37b5b83a5db7f93eca2dfa4e
frontierpsycho <nonsenseisay@protonmail.com>
2021-07-02 17:08:15 +0200

    Massaged TODOs, used print-card.

--- set.lisp
+++ set.lisp
@@ -6,6 +6,7 @@
 
 (defvar *shapes* '(oval diamond squiggle))
 
+;; TODO random values if not provided
 (defun make-card (number shading colour shape) (list :number number :shading shading :colour colour :shape shape))
 
 ;; TODO do better, skip field names
@@ -13,9 +14,6 @@
 
 (defun get-card-number-index (card) (position (getf card :number) *numbers*))
 (defun compare-card-number-index (card1 card2) (< (get-card-number-index card1) (get-card-number-index card2)))
-
-;; TODO write a macro that produces get-card-X-index for all attributes?
-;; TODO write a generic compare?
 
 (defun sort-cards (card-list)
   (sort card-list #'compare-card-number-index))
@@ -69,14 +67,10 @@
 (defmacro extrapolate-attribute (card1 card2 attribute)
   (list `(car (set-difference ,(format NIL "*~(~a~)s*" attribute) (extract-attribute card1 card2 attribute)))))
 
-;; collect attributes
-
 ;;; Use all of the above
+
 (let ((card1 (make-card (nth 1 *numbers*) (nth 1 *shadings*) (nth 0 *colours*) (nth 0 *shapes*)))
       (card2 (make-card (nth 0 *numbers*) (nth 1 *shadings*) (nth 1 *colours*) (nth 2 *shapes*)))
       (card3 (make-card (nth 2 *numbers*) (nth 1 *shadings*) (nth 2 *colours*) (nth 1 *shapes*)))
       (card4 (apply #'make-card '(one outlined purple oval))))
-  (format
-    t
-    "~a"
-    (extrapolate-card card1 card2)))
+  (print-card (extrapolate-card card1 card2)))
